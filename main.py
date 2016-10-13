92c6cd5feb19ebe7acfdd336cf0ba2d0de4788b9
kolin jacobs <kolinjacobs@yahoo.com>
2016-10-13 13:24:03 -0700

    class name change

--- main.py
+++ main.py
@@ -36,13 +36,13 @@ class BlogPosts(db.Model):
     votes = db.IntegerProperty(required=True)
     voters = db.ListProperty(str)
 #an entity for blog users
-class users(db.Model):
+class Users(db.Model):
     username = db.StringProperty(required=True)
     email = db.EmailProperty(required=True)
     password_hash = db.StringProperty(required=True)
     profile_picture = db.BlobProperty(required=False)
 #an entity for comments
-class comments(db.Model):
+class Comments(db.Model):
     username = db.StringProperty(required=True)
     post_id = db.StringProperty(required=True)
     content = db.TextProperty(required=True)
@@ -56,7 +56,7 @@ class MainPage(Handler):
         posts = db.GqlQuery("SELECT * FROM BlogPosts "
                             "ORDER BY created DESC ")
         user = self.get_user_cookie()
-        blog_users = users.all()
+        blog_users = Users.all()
         self.render("index.html",
                     posts = posts,
                     user = user)
@@ -145,7 +145,7 @@ class Post_Blog(Handler):
                             title=title,
                             content=content )
 
-class login(Handler):
+class Login(Handler):
     def get(self):
         self.render("login.html")
     def post(self):
@@ -153,9 +153,9 @@ class login(Handler):
         password = self.request.get("password")
         user_exist = False
         pw_hash = ""
-        users = db.GqlQuery("SELECT * FROM users")
+        Users = db.GqlQuery("SELECT * FROM Users")
         #checks if user exists
-        for user in users:
+        for user in Users:
             if username == user.username:
                 user_exist = True
                 break
@@ -179,7 +179,7 @@ class login(Handler):
             self.render("login.html",
                         username_error=username_error,
                         password_error=pass_error)
-class signup(Handler):
+class Signup(Handler):
     def get(self):
         self.render("signup.html")
     def post(self):
@@ -190,7 +190,7 @@ class signup(Handler):
         email = self.request.get("email")
         password = self.request.get("password")
         password_two = self.request.get("password_two")
-        blog_users = db.GqlQuery("SELECT * FROM users")
+        blog_users = db.GqlQuery("SELECT * FROM Users")
         #checks for form validation and adds user to database
         for user in blog_users:
             if username == user.username:
@@ -202,7 +202,7 @@ class signup(Handler):
         if username and email and password and password_two and \
                         username_taken == False and email_taken == False and pass_match == True:
             pass_hash = hash.make_pw_hash(username,password)
-            a = users(username=username,
+            a = Users(username=username,
                       password_hash=pass_hash,
                       email=email)
             a.put()
@@ -230,7 +230,7 @@ class signup(Handler):
                         username_error=username_error,
                         email_error=email_error,
                         password_error=password_error)
-class logout(Handler):
+class Logout(Handler):
     def get(self):
         self.response.headers.add_header('Set-Cookie', 'user=""')
         self.redirect("/")
@@ -241,8 +241,8 @@ class posted_blog(Handler):
     def generate_comment_id(self):
         is_unique = True
         id = hash.make_salt(9)
-        all_comments = comments.all()
-        for comment in all_comments:
+        all_comments = Comments.all()
+        for comment in all_Comments:
             if comment.id == id:
                 is_unique = False
         if is_unique == False:
@@ -254,7 +254,7 @@ class posted_blog(Handler):
         user = self.get_user_cookie()
         id = self.request.get("q")
         posts = BlogPosts.all()
-        all_comments = comments.all()
+        all_comments = Comments.all()
         post_comments = []
         for post in posts:
             if str(id) == str(post.id):
@@ -276,7 +276,7 @@ class posted_blog(Handler):
         if user != "":
             if delete_Button:
                 # gets comment by id and deletes it
-                all_comments = comments.all()
+                all_comments = Comments.all()
                 for c in all_comments:
                     #verifiest that the user owns this comment before deleting
                     if c.id == comment_id and c.username == user:
@@ -286,13 +286,13 @@ class posted_blog(Handler):
                 # checks for validation and adds comment
                 id = self.generate_comment_id()
                 content = self.request.get("commentContent")
-                a = comments(username= user,post_id = str(post_id),content=content,id=id)
+                a = Comments(username= user,post_id = str(post_id),content=content,id=id)
                 a.put()
                 self.redirect("/posted?q="+str(post_id))
         else:
             self.redirect("/login")
 #handler for the user profile
-class profile(Handler):
+class Profile(Handler):
     def get(self):
         user = self.get_user_cookie()
         if user != "":
@@ -307,8 +307,8 @@ class profile(Handler):
     def post(self):
         #checks for user input for delete or edit
         cur_user = self.get_user_cookie()
-        all_users = users.all()
-        all_comments = comments.all()
+        all_users = Users.all()
+        all_comments = Comments.all()
         post_id= self.request.get("delete")
         edit_Button = self.request.POST.get('edit_item', None)
         delete_Button = self.request.POST.get('delete_item', None)
@@ -327,7 +327,7 @@ class profile(Handler):
         else:
             self.redirect("/login")
 
-class account(Handler):
+class Account(Handler):
     #gets blog posts from a user id and renders the posts
     def get(self):
         cur_user = self.get_user_cookie()
@@ -341,7 +341,7 @@ class account(Handler):
             self.render("account.html", posts=profile_post, user=user)
         else:
             self.redirect("/profile")
-class edit(Handler):
+class Edit(Handler):
     #passes blog id and fills title and content forms
     def get(self):
         user = self.get_user_cookie()
@@ -382,10 +382,10 @@ app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/post', Post_Blog),
     ('/posted', posted_blog),
-    ('/login', login),
-    ('/logout', logout),
-    ('/signup', signup),
-    ('/profile', profile),
-    ('/account', account),
-    ('/edit', edit)],
+    ('/login', Login),
+    ('/logout', Logout),
+    ('/signup', Signup),
+    ('/profile', Profile),
+    ('/account', Account),
+    ('/edit', Edit)],
     debug=True)
